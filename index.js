81c818ad8fdbba2e872cbf8feb36b0f459ac1525
Lucca Alves <lucca.al@hotmail.com>
2023-11-21 15:58:23 -0300

    Seleção de dado individual

--- index.js
+++ index.js
@@ -39,6 +39,25 @@ app.post("/register/save", (req, res)=> {
     })
 })
 
+app.get("/book/:id", (req, res)=> {
+    const id = request.params.id
+
+    const sql = `
+        SELECT * FROM books
+        WHERE id=${id}
+    `
+
+    conn.query(sql, (error, data) =>{
+        if(error){
+            return console.log(error)
+        }
+
+        const book = data[0]
+
+        response.render("book", {book})
+    })
+})
+
 app.get('/register', (req, res)=>{
     res.render("register")
 }) 
